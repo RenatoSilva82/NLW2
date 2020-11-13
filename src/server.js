d318ace036aaed74811815ade0df42bd38db7838
Flávio Renato <renatoarapiraca.82@gmail.com>
2020-11-12 22:36:40 -0300

    Iniciando o javascript do projeto. Utilizando xpress e nunjucks

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const proffys = [
     {
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "82999658563",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        whatsapp: "82999350146",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Qímica",
-        cost: "20",
+        cost: "20,00",
         weekday: [0],
         time_from: [720],
         time_to: [1220]
@@ -14,39 +14,66 @@ const proffys = [
         name: "Daniele Evangelista",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "82999658563",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Qímica",
-        cost: "20",
+        cost: "75,00",
         weekday: [1],
         time_from: [720],
         time_to: [1220]
     }
 ]
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Serxta-feira",
+    "Sábado",
+]
 
 function pageLanding(req,res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 }
 
 
 const express = require('express') // Importa o Express
-const server = express()
+const server = express() // Inicia o servidor express
+const nunjucks = require('nunjucks') // Importa nunjucks
 
-server.use(express.static("public")) // configura a pasta public como a pasta principal
+// Configura nunjucks
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
+server
+.use(express.static("public")) // configura a pasta public como a pasta principal
 .get("/", pageLanding)
-
 .get("/study", pageStudy)
-
 .get("/give-classes", pageGiveClasses)
-
 .listen(5500)
 
 // aula 04 - 1:04
